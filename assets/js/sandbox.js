76d7366470ff5af037a20e0d0632e80fec3d0652
Alena.Kygai <alena.kygai@gmail.com>
2021-12-24 23:17:59 +0300

    Add Adder

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,13 +1,27 @@
 'use strict';
 
 
-function createCounter(initial=0){
-  let i=initial; // замкнутая переменная 
-  return function counter(){
-    return i++;
-  }
-}
+// function createCounter(initial=0){
+//   let i=initial; // замкнутая переменная 
+//   return {
+//     increment(){
+//       return ++i;
+//     },
+//     decrement(){
+//       return --i;
+//     }
+//   }
+// }
 
-const counter1 = createCounter();
-console.log(counter1());
-const counter2 = createCounter();
+// const counter1 = createCounter(10);
+// console.log(counter1.decrement());
+// console.log(counter1.increment());
+// const counter2 = createCounter();
+
+
+const createAdder = (n) => (m) => (n += m);
+
+const adder = createAdder(10);
+console.log(adder(4));
+console.log(adder(3));
+console.log(adder(10));
