f29d336a3f0ec04c4198a568b5d0f1c544eb90b9
Alena.Kygai <alena.kygai@gmail.com>
2021-12-30 02:28:46 +0300

    Buttons change bg-color

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,16 +2,14 @@
 
 const btns = document.querySelectorAll('button');
 
-// console.log(btns[0].getAttribute('data-path'));
-
-// for (const btn of btns) {
-//   btn.addEventListener('click', ({target})=>{
-//     // console.log(target.getAttribute('data-path'));
-//     console.log(target.dataset.path);
-//   })
-// }
-const btnHandler = ({target:{dataset:{path}}})=>{ console.log(path);}
+const changeColor = ({
+  target:{
+    dataset:{color},
+    parentNode: parentRoot,
+}})=>{
+  parentRoot.style.backgroundColor = color;
+}
 
 for (const btn of btns) {
-  btn.addEventListener('click',btnHandler);
+  btn.addEventListener('click', changeColor);
 }
