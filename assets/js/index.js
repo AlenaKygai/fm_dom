685515059cb079ffd7273b15b1ae7c8903f858d4
Alena.Kygai <alena.kygai@gmail.com>
2021-12-30 03:29:11 +0300

    Use {capture:true, once:true}

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,22 @@
 const btns = document.querySelectorAll('button');
 
 const changeColor = ({
-  target:{
-    dataset:{color},
+  target: {
+    dataset: { color },
     parentNode: parentRoot,
-}})=>{
+  }
+}) => {
   parentRoot.style.backgroundColor = color;
+}
+const handler = ({currentTarget})=>{
+  console.log(currentTarget , 'dipping');
 }
 
 for (const btn of btns) {
   btn.addEventListener('click', changeColor);
 }
+
+document.getElementById('root').addEventListener('click', handler, {capture:true, once:true});
+document.body.addEventListener('click', handler, true);
+document.addEventListener('click', handler, true);
+window.addEventListener('click', handler, true);
