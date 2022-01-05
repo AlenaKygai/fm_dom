66cdd57b143d2e7d7ecb836d2a4424cab2df6687
Alena.Kygai <alena.kygai@gmail.com>
2022-01-05 23:51:56 +0300

    Add stringToColour & handelImageError

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,10 +11,20 @@ function createActorCards(actor){
   const container = document.createElement('article');
   container.classList.add('cardContainer');
 
+  const imgWrapper = document.createElement('div');
+  imgWrapper.classList.add('cardImageWrapper');
+
+  const initials = document.createElement('div');
+  initials.classList.add('initials');
+  initials.append(document.createTextNode(actor.name[0]));
+
   const img = document.createElement('img');
   img.classList.add('cardImage');
   img.setAttribute('src', actor.photo);
   img.setAttribute('alt', actor.name);
+  img.addEventListener('error', handelImageError);
+
+  imgWrapper.append(initials,img)
 
   const name = document.createElement('h2');
   name.classList.add('cardName');
@@ -25,12 +35,28 @@ function createActorCards(actor){
   description.classList.add('cardDescription');
   description.append(document.createTextNode(actor.birthdate));
 
-  container.append(img,name,description);
+  container.append(imgWrapper,name,description);
   card.append(container);
   return card;
 }
 
-// for (const element of HTMLElements) {
-//   cardsContainer.appendChild(element);
-// }
 cardsContainer.append(...HTMLElements);
+
+/* handels */
+function handelImageError({target}){
+  target.remove();
+}
+
+/*  */
+var stringToColour = function(str) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  var colour = '#';
+  for (var i = 0; i < 3; i++) {
+    var value = (hash >> (i * 8)) & 0xFF;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
+}
