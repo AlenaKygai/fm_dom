aacd32a8dee0d3a3abd63a6fc6ebd7840e8c424f
Alena.Kygai <alena.kygai@gmail.com>
2022-01-18 17:09:20 +0300

    Refactor

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,22 +11,6 @@ function createActorCards(actor){
   const container = document.createElement('article');
   container.classList.add('cardContainer');
 
-  const imgWrapper = document.createElement('div');
-  imgWrapper.classList.add('cardImageWrapper');
-
-  const initials = document.createElement('div');
-  initials.classList.add('initials');
-  initials.append(document.createTextNode(getInitials(actor.name) || 'noname'));
-  initials.style.backgroundColor = stringToColour(actor.name || '');
-
-  const img = document.createElement('img');
-  img.classList.add('cardImage');
-  img.setAttribute('src', actor.photo);
-  img.setAttribute('alt', actor.name);
-  img.addEventListener('error', handelImageError);
-
-  imgWrapper.append(initials,img)
-
   const name = document.createElement('h2');
   name.classList.add('cardName');
   // name.textContent = actor.name;
@@ -36,16 +20,49 @@ function createActorCards(actor){
   description.classList.add('cardDescription');
   description.append(document.createTextNode(actor.birthdate || 'unknow'));
 
-  container.append(imgWrapper,name,description);
+  container.append(createImageWrapper(actor),name,description);
   card.append(container);
   return card;
 }
 
 cardsContainer.append(...HTMLElements);
 
+function createImageWrapper(actor){
+  const {id,name} = actor;
+  const imgWrapper = document.createElement('div');
+  imgWrapper.setAttribute('id',`wrapper${id}`);
+  imgWrapper.classList.add('cardImageWrapper');
+
+  const initials = document.createElement('div');
+  initials.classList.add('initials');
+  initials.append(document.createTextNode(getInitials(name || 'noname')));
+  initials.style.backgroundColor = stringToColour(name || '');
+
+  imgWrapper.append(initials,createImage(actor));
+  return imgWrapper;
+}
+
+function createImage({photo,name,id}){
+  const img = document.createElement('img');
+  img.dataset.id = id;
+  img.classList.add('cardImage');
+  img.setAttribute('src', photo);
+  img.setAttribute('alt', name);
+  img.addEventListener('error', handelImageError);
+  img.addEventListener('load', handelImageLoad);
+  return img;
+}
+
 /* handels */
 function handelImageError({target}){
   target.remove();
+}
+
+function handelImageLoad({target,
+  target:{
+    dataset:{id}
+  }}){
+  document.getElementById(`wrapper${id}`).append(target);
 }
 
 /* utilits */
@@ -61,7 +78,6 @@ function stringToColour(str){
   }
   return colour;
 }
-
 
 function getInitials(fullName){
   const [name, sName] = fullName.trim().split(' ');
