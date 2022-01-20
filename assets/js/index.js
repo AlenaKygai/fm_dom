8ca2823078cfae3450e3845679a2927b00c218dd
Alena.Kygai <alena.kygai@gmail.com>
2022-01-21 00:46:30 +0300

    Add button delete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,50 @@
 'use strict';
 
-const messageArray = [];
-
+const state = [];
 const form = document.getElementById('root-form');
-const ul = document.getElementById('lists');
+const list = document.getElementById('list');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const { target: { textInput} } = e;
-
   const inputValue = textInput.value.trim();
-  // const validator = /^[A-Z][a-z]{3,12} [A-Z][a-z]{2,17}$/;
-  const validatorForImages = /^[^><"'?!\\ \/:;*\|«+]*\.(jpg|png)$/;
-  //добавлять значение инпута в массив, если оно не пустое
-  //и соответствует строке типа Name Sname
-  if(validatorForImages.test(inputValue)) {
-      messageArray.push(inputValue);
-      const li = createElement('li', { classNames:['item'] }, inputValue);
-      ul.append(li);
-      // после добавления - очищать форму
-      form.reset();
-      textInput.focus();
+  const validatorName = /^[A-Z][a-z]{2,17}$/;
+  if(validatorName.test(inputValue) && !state.includes(inputValue)) {
+    state.push(inputValue);
+    form.reset();
+    const li = createElement('li', {classNames:['item']}, document.createTextNode(inputValue));
+    const btn = createElement('button', 
+      {dataValue:inputValue , typeEvent: 'click', onClick:deleteHandler.bind(li)}, 
+      document.createTextNode('X'));
+    li.append(btn);
+    list.append(li); 
+    textInput.focus(); 
     }else{
       console.log('Wrong format data');
     }
 })
 
-function createElement(type,{classNames}, ...children){
+/* 
+добавить кнопку удаления для каждого пункта списка
+по нажатию на кнопку удалять 
+- пункт списка со страницы
+-значение из массива
+*/
+
+function deleteHandler({target:{dataset:{value}}}){
+  this.remove();
+  state.splice(state.indexOf(value),1);
+}
+
+function createElement(type,{dataValue = '', classNames=[], typeEvent= '', onClick=null}, ...children){
   const elem = document.createElement(type);
-  elem.classList.add(...classNames);
+  if(dataValue){
+    elem.dataValue.value = dataValue;
+  }
+  if(classNames.length){
+    elem.classList.add(...classNames);
+  }
+  elem.addEventListener(typeEvent, onClick); // for example
   elem.append(...children);
   return elem;
 }
