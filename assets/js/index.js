334d3ecbdce8f4b3c4871a72c4357f36bb20a57e
Alena.Kygai <alena.kygai@gmail.com>
2022-01-21 17:43:04 +0300

    Add counter for home task

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,8 +6,26 @@ function btnHandler(e){
   
 }
 
-console.log(1);
-setTimeout(()=>{
-  console.log(3);
-},0)
-console.log(2);
+
+const identificatore = setTimeout(()=>{
+  console.log('Hi');
+},100)
+
+clearTimeout(identificatore);
+
+
+
+/* 
+
+функция которая выводит последовательно числа от 1 до 10 через 0,5 секунды 
+*/
+
+function counter(){
+  for (let i = 1; i <= 10; i++) {
+      (function(index) {
+          setTimeout(function() { console.log(i) }, i * 1000);
+      })(i);
+  }
+}
+counter();
+
