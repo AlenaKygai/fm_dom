e4940e067d49353a7a1a004e41aa23cc7d02fa86
Alena.Kygai <alena.kygai@gmail.com>
2022-01-21 23:59:12 +0300

    Variant for counter

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,12 +20,33 @@ clearTimeout(identificatore);
 функция которая выводит последовательно числа от 1 до 10 через 0,5 секунды 
 */
 
-function counter(){
+function counterFirsVariant(){
   for (let i = 1; i <= 10; i++) {
       (function(index) {
           setTimeout(function() { console.log(i) }, i * 1000);
       })(i);
   }
 }
-counter();
+// counterFirsVariant();
 
+function counterSecondVariant(i){
+  setTimeout(()=>{
+    console.log(i);
+      if(i<10) {
+          counterSecondVariant(++i);
+      }
+    },500)
+}
+// counterSecondVariant(1);
+
+
+function counterThirdVariant(i){
+  setTimeout(() => {
+    console.log(i)
+    if(i === 0) {
+      counterThirdVariant(--i);
+    }
+  }, 500);
+}
+
+// counterThirdVariant(10);
